bc8f17cb759410f6d6cb67a6339a6e66ff059055
Bianca Aspin <bianca@mj94.net>
2023-02-14 11:59:17 -0800

    Render saved questions and answers on 'Saved Clues' page.

--- my-app/src/Components/SavedClues.js
+++ my-app/src/Components/SavedClues.js
@@ -1,7 +1,26 @@
+import {useState, useEffect} from "react";
+
 function SavedClues() {
+
+    const [savedClues, setSavedClues] = useState([])
+
+    useEffect(() => {
+        fetch("http://localhost:4000/saved")
+        .then(response => response.json())
+        .then(clueData => setSavedClues(clueData))
+    }, [])
+
+    const savedClueItems = savedClues.map(clue => {
+        return <li>Question: {clue.question} Answer: {clue.answer}</li>
+    })
+
+
     return(
         <div>
             <h2>Saved Clues</h2>
+            <ul>
+                {savedClueItems}
+            </ul>
         </div>
     )
 }
