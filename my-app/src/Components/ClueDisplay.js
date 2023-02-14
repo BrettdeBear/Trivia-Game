1dc7a8a40d66f5e0fd0622635bc0a3039217b10e
Bianca Aspin <bianca@mj94.net>
2023-02-14 11:26:10 -0800

    Update clue display with 'Save Clue' button and POST to 'saved'.

--- my-app/src/Components/ClueDisplay.js
+++ my-app/src/Components/ClueDisplay.js
@@ -26,12 +26,26 @@ function ClueDisplay({clue, points, setPoints, showAnswer, setShowAnswer}) {
         setPoints(points => points - clue.value)
     }
 
+    function handleSave() {
+        fetch("http://localhost:4000/saved", {
+            method: "POST",
+            headers: {
+                "content-type": "application/json"
+            },
+            body: JSON.stringify(clue) 
+            })
+            .then(response => response.json())
+            .then(savedClue => console.log(savedClue))
+        }
+
+
     return (
         <div>
             <p>{clue.question}</p>
             {showAnswer ? <p>{clue.answer}</p> : null}
             <button onClick={handleClick}>{buttonText}</button>
             {showPointsButton}
+            <button onClick={handleSave}>Save Clue</button>
         </div>
     )
 }
