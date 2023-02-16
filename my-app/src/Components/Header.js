e8ee6a5ccc262346baee9c82c934b9fc2fa334e7
Brett de Bear <bkdebear@gmail.com>
2023-02-16 10:02:33 -0800

    css styling, margins

--- my-app/src/Components/Header.js
+++ my-app/src/Components/Header.js
@@ -4,7 +4,7 @@ import PointsDisplay from "./PointsDisplay";
 
 function Header() {
    return (
-    <div>
+    <div className="header">
         <h1>Peril! The Code-Blooded Trivia Game</h1>
         <NavBar />
     </div>
