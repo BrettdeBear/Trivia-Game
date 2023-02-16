51b3fb59c4fecd2fa2447a859c885d34187fa96a
Bianca Aspin <bianca@mj94.net>
2023-02-16 09:27:20 -0800

    Add Felix Quebec image to header.

--- my-app/src/Components/Header.js
+++ my-app/src/Components/Header.js
@@ -1,11 +1,17 @@
 import NavBar from "./NavBar";
 import PointsDisplay from "./PointsDisplay";
+import Felix from "../assets/felix.png"
+
+
+
+
 
 
 function Header() {
    return (
     <div>
         <h1>Peril! The Code-Blooded Trivia Game</h1>
+        <img src={Felix} alt="Felix Quebec" />
         <NavBar />
     </div>
    )
