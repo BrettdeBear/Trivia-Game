8fecf1c79a14d4f092a232d9f0c853b1e3947de8
Bianca Aspin <bianca@mj94.net>
2023-02-16 09:45:24 -0800

    Add updated artwork for Felix Quebec.

--- my-app/src/Components/Header.js
+++ my-app/src/Components/Header.js
@@ -1,17 +1,13 @@
 import NavBar from "./NavBar";
 import PointsDisplay from "./PointsDisplay";
-import Felix from "../assets/felix.png"
-
-
-
-
+import Felix from "../assets/felixWelcome.png"
 
 
 function Header() {
    return (
-    <div>
+    <div id="header">
         <h1>Peril! The Code-Blooded Trivia Game</h1>
-        <img src={Felix} alt="Felix Quebec" />
+        <img src={Felix} alt="Dapper stick figure saying 'I'm your host, Felix Quebec!" />
         <NavBar />
     </div>
    )
