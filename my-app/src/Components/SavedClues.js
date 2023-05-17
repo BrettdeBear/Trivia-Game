c770d1e8ca4c90f479652c4e8870dd9f6b040f6d
Bianca Aspin <bianca@mj94.net>
2023-05-17 12:46:17 -0700

    Fix accessibility issue with 'Saved Clue' list structure.

--- my-app/src/Components/SavedClues.js
+++ my-app/src/Components/SavedClues.js
@@ -2,14 +2,16 @@ function SavedClues({savedClues, setSavedClues}) {
 
     const savedClueItems = savedClues.map(clue => {
         return (
-        <ul className="savedClue" key={clue.id}>
-            <li><strong>Question: </strong> {clue.question}</li>
-            <li><strong>Answer: </strong> <em>{clue.answer}</em></li>
-            <li><strong>Value: </strong>${clue.value}</li>
-            <li><strong>Category: </strong>{clue.category.toUpperCase()}</li>
-            <br/>     
+        <div className="savedClue" key={clue.id}>
+            <ul>
+                <li><strong>Question: </strong> {clue.question}</li>
+                <li><strong>Answer: </strong> <em>{clue.answer}</em></li>
+                <li><strong>Value: </strong>${clue.value}</li>
+                <li><strong>Category: </strong>{clue.category.toUpperCase()}</li>
+            </ul>
+            <br />  
             <button onClick={() => handleDelete(clue.id)}>Delete</button>
-        </ul>
+        </div>
     )})
 
     function handleDelete(deletedClueId) {
